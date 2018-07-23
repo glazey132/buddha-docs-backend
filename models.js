4d6838a5259f25bf50457d910458fdbb400e6311
Alex <alglaze132@gmail.com>
2018-07-22 18:56:30 -0700

    docschema

--- models.js
+++ models.js
@@ -11,3 +11,35 @@ const UserSchema = new mongoose.Schema({
     required: true
   }
 });
+
+const DocSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    default: 'untitled'
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Number,
+    default: new Date().getTime()
+  },
+  contents: {
+    // most recent state
+    type: String,
+    default: ''
+  },
+  last_edit: {
+    type: Number,
+    default: new Date().getTime() // updates on doc save
+  },
+  collaborators: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  revision_history: {
+    type: Array,
+    default: [] //updates on doc save
+  }
+});
