96e79f94b53fbdf0a9a7770ccb282d7641c2942f
Alex <alglaze132@gmail.com>
2018-07-22 19:31:42 -0700

    passport and local strategy

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+LocalStrategy = require('passport-local').Strategy;
