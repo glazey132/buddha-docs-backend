18f961dcb02bd10898536abf5d205f82d3b44786
Alex <alglaze132@gmail.com>
2018-07-25 22:41:47 -0700

    change cors headers

--- server.js
+++ server.js
@@ -38,12 +38,12 @@ app.use(passport.session());
 ** CORS settings
 */
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', 'https://localhost:8080');
-  res.header('Access-Control-Allow-Methods', 'OPTIONS, POST, GET');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-type, Accept'
+    'Origin, X-Requested-With, Content-Type, Accept'
   );
   next();
 });
